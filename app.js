3ebc8210fab334333c4146378552f7beb41694a3
PabloTei <pteijeirolopez@gmail.com>
2023-01-11 00:41:57 +0100

    iteration1

---
+++ app.js
@@ -0,0 +1,12 @@
+// Iteración 1: Usa includes
+
+const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta'];
+
+for (const element of products) {
+    
+    if(element.includes("Camiseta")) {
+        console.log(element);
+    }
+
+}
+
