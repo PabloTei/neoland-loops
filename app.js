57f482538542e04af5ecc19e08c50037db84db17
PabloTei <pteijeirolopez@gmail.com>
2023-01-11 11:35:34 +0100

    iteracion2

--- app.js
+++ app.js
@@ -10,3 +10,37 @@ for (const element of products) {
 
 }
 
+ // Iteración 2: Condicionales avanzados
+
+const alumns = [
+        {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
+		{name: 'Lucia Aranda', T1: true, T2: false, T3: true},
+		{name: 'Juan Miranda', T1: false, T2: true, T3: true},
+		{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
+		{name: 'Raquel Benito', T1: true, T2: true, T3: true}
+]
+
+alumns.forEach((alumns) => {
+
+    if (alumns.T1 && alumns.T2) {
+        
+        alumns.isApproved = true;
+
+    
+    } else if (alumns.T2 && alumns.T3) {
+
+        alumns.isApproved = true;
+
+    } else if (alumns.T1 && alumns.T3) {
+
+        alumns.isApproved = true;
+        
+    } else {
+    
+        alumns.isApproved = false;
+    }
+
+  
+console.log(alumns);
+
+})
